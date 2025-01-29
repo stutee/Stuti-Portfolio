080f24e443c659de30c6dac84a97f86f3e5980a5
stutee <stutikafle6@gmail.com>
2025-01-28 23:27:49 -0800

    projects added

--- components/Project.jsx
+++ components/Project.jsx
@@ -1,6 +1,18 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 import uh_logo from "../assets/UH_logo.png";
+import drone from "../assets/drone.png";
+import pulchowk_team from "../assets/pulchowk_team.png";
+import elephant_intrusion from "../assets/elephant_intrusion.png";
+import vrlab from "../assets/vrlab.png";
+import movie_website from "../assets/movie_website.png";
+import certificate_lerobot from "../assets/certificate_lerobot.png";
+import lerobot_view from "../assets/lerobot_view.png";
+import lerobot_team from "../assets/lerobot_team.png";
+import foodie_chatbot from "../assets/foodie_chatbot.png";
+import watering_system from "../assets/watering_system.png";
+import poster_presentation from "../assets/poster_presentation.png";
+
 
 const Projects = () => {
   return (
@@ -20,14 +32,128 @@ const Projects = () => {
         Projects
       </motion.h2>
 
-      {/* Project 1 */}
+      {/* Project Reinforcement Learning */}
       <motion.div
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 0.8 }}
         className="mb-16"
       >
-        <h3 className="text-2xl font-bold mb-4">
+        <h3 className="text-xl mb-4 font-semibold text-gray-600">
+        Reinforcement Learning with Human Feedback for fine tuning Large Language Model (LLM) (2024)     
+        </h3>
+        <ul className="list-disc ml-6 mb-4 text-gray-600 dark:text-white/80">
+          <li>
+            Fine-tuned OpenAI GPT-2 using Reinforcement Learning from Human Feedback (RLHF) with algorithms like Proximal Policy Optimization, Actor to critic, and Trust Region Policy Optimization, optimizing sentiment alignment on the IMDB dataset. Trained supervised reward model to give scores.
+
+          </li>
+          <li>
+          Comparative analysis of the algorithms using training efficiency, computational complexity, and reward model scores.
+          </li>
+        </ul>
+          <div className="relative w-full ml-[300px]">
+            <Image
+              src={poster_presentation}
+              alt="drone"
+              width={500}
+              height={500}
+              className="rounded-md"
+            />
+          </div> 
+      </motion.div>
+
+        {/* AWS cloud computing */}
+        <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.8 }}
+        className="mb-16"
+      >
+        <h3 className="text-xl mb-4 font-semibold text-gray-600">
+        AWS cloud based Movie Recommendation System (2024)     
+        </h3>
+        <ul className="list-disc ml-6 mb-4 text-gray-600 dark:text-white/80">
+          <li>
+          Developed a real-time movie recommendation system leveraging AWS cloud services, SageMaker for machine learning, used K-nearest Neighbor algorithm and ECS (Fargate and EC2) for containerized microservices deployment. 
+
+          </li>
+          <li>
+          Designed a user-friendly frontend with Next.js, integrated with secure authentication via Amazon Cognito, and implemented CI/CD pipelines using Docker and GitHub Actions.
+          </li>
+        </ul>
+          <div className="relative w-full ml-[300px]">
+            <Image
+              src={movie_website}
+              alt="drone"
+              width={500}
+              height={500}
+              className="rounded-md"
+            />
+          </div> 
+      </motion.div>
+
+      {/* Foodie Chatbot */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.8 }}
+        className="mb-16"
+      >
+        <h3 className="text-xl mb-4 font-semibold text-gray-600">
+        Foodie Chatbot(2024)    
+        </h3>
+        <ul className="list-disc ml-6 mb-4 text-gray-600 dark:text-white/80">
+          <li>
+          Created an AI chatbot using OpenAI's Assistant API that acts as a guide for choosing restaurants in Houston.
+          </li>
+          <li>
+          Used PyQt to create a GUI for the chatbot.
+          </li>
+        </ul>
+          <div className="relative w-full ml-[300px]">
+            <Image
+              src={foodie_chatbot}
+              alt="drone"
+              width={400}
+              height={300}
+              className="rounded-md"
+            />
+          </div> 
+      </motion.div>
+
+      {/* Energy modeling */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.8 }}
+        className="mb-16"
+      >
+        <h3 className="text-xl mb-4 font-semibold text-gray-600">
+        Energy Prediction Modeling: Enhancing AdaBoost Regressor with GA-PSO Hybrid Optimization (2023)     
+        </h3>
+        <ul className="list-disc ml-6 mb-4 text-gray-600 dark:text-white/80">
+          <li>
+          Employed the "Smart Home Dataset with Weather Information" from Kaggle, featuring minute-by-minute energy consumption Preprocessed data by removing empty rows, adding time indices, renaming columns for clarity, removing duplicates using pandas. 
+
+          </li>
+          <li>
+          Chose AdaBoostRegressor for its ensemble learning capability, applied a hybrid approach combining GA for broad exploration and PSO for precise hyperparameter tuning, enhancing the model's predictive accuracy.
+          </li>
+          <li>
+          Improved AdaBoost model’s performance significantly post-optimization, achieving lower Mean Squared Error (MSE) and higher R-squared value compared to the initial implementation.
+          </li>
+        </ul>
+      </motion.div>
+
+
+      {/* Project UAV */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.8 }}
+        className="mb-16"
+      >
+        <h3 className="text-xl mb-4 font-semibold text-gray-600">
           ROS-Based Geofencing and Path Planning Optimization for UAVs (2021)
         </h3>
         <ul className="list-disc ml-6 mb-4 text-gray-600 dark:text-white/80">
@@ -46,71 +172,59 @@ const Projects = () => {
           </li>
         </ul>
         {/* Images */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          <div className="relative w-full h-64">
+        <div className="grid grid-cols-1 sm:grid-cols-2">
+          <div className="relative w-full ml-[100px]">
             <Image
-              src={uh_logo}
-              alt="UAV Hardware"
-              fill
-              className="object-cover rounded-md"
+              src={drone}
+              alt="drone"
+              width={400}
+              height={300}
+              className="rounded-md"
             />
           </div>
-          <div className="relative w-full h-64">
+          <div className="relative w-full -ml-10">
             <Image
-              src="/images/uav-project-2.jpg"
+              src={pulchowk_team}
               alt="UAV Team"
-              fill
-              className="object-cover rounded-md"
+              width={400}
+              height={300}
+              className="rounded-md"
             />
           </div>
         </div>
       </motion.div>
 
-      {/* Project 1 */}
-      <motion.div
+        {/* Elephant Intrusion */}
+        <motion.div
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 0.8 }}
         className="mb-16"
       >
-        <h3 className="text-2xl font-bold mb-4">
-          ROS-Based Geofencing and Path Planning Optimization for UAVs (2021)
+        <h3 className="text-xl mb-4 font-semibold text-gray-600">
+        Elephant Intrusion Detection and Alerting (2020)    
         </h3>
         <ul className="list-disc ml-6 mb-4 text-gray-600 dark:text-white/80">
           <li>
-            The project experimented with physical hardware implementation and
-            software simulation in CoppeliaSim along with Robot Operating
-            System (ROS). Triangle Width Characterization with Adjacency (TWCA)
-            for optimizing the breach detection problem.
+          The isolation Forest algorithm for detecting the anomaly frequency of Elephants, IMU sensors, low pass filters, and energy thresholding to perform detection was used along with the STM32 microcontroller.
+
           </li>
           <li>
-            My Role: Research on algorithms for effective breach detection. Ray
-            Casting algorithm and TWCA were tested as they supported our
-            dataset. TWCA showed better accuracy and was chosen. Similarly, I
-            implemented Breadth First Search (BFS) for the shortest distance
-            travel of the UAV.
+          My role: Research anomaly detection algorithm, implement it on our dataset, and provide the best algorithm. According to the dataset, Random Forest algorithm and Isolation Forest Algorithm performed best. However, the Isolation Forest Algorithm was chosen for its better F1 score, less variance of data, and less expensive in terms of time complexity.
           </li>
         </ul>
-        {/* Images */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          <div className="relative w-full h-64">
+          <div className="relative ml-[300px]">
             <Image
-              src={uh_logo}
-              alt="UAV Hardware"
-              fill
-              className="object-cover rounded-md"
+              src={elephant_intrusion}
+              alt="elephant"
+              width={500}
+              height={10}
+              className="rounded-md"
+              style={{ objectFit: "cover", height: "400px" }}
             />
-          </div>
-          <div className="relative w-full h-64">
-            <Image
-              src="/images/uav-project-2.jpg"
-              alt="UAV Team"
-              fill
-              className="object-cover rounded-md"
-            />
-          </div>
-        </div>
+          </div> 
       </motion.div>
+
 
     </motion.div>
   );
