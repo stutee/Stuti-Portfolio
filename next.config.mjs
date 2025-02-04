6c4a5ed78469d383589b1fc8777595f134298fb8
stutee <stutikafle6@gmail.com>
2025-02-04 11:27:13 -0800

    update next.js routing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    basePath: "/Stuti-Portfolio",
+    assetPrefix: "/Stuti-Portfolio",
+};
 
 export default nextConfig;
